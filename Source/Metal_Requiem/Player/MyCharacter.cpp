4ef3d80e8b935f643bc49a98440f7f828cf5d3d5
Akik4 <dupontstef80@gmail.com>
2023-11-23 16:36:08 +0100

    feat: Animation added

--- Source/Metal_Requiem/Player/MyCharacter.cpp
+++ Source/Metal_Requiem/Player/MyCharacter.cpp
@@ -7,12 +7,43 @@
 #include "GameFramework/SpringArmComponent.h" 
 #include "GameFramework/PawnMovementComponent.h" 
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Engine.h"
 
 // Sets default values
 AMyCharacter::AMyCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	//Setup MESH
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> Skeletal(TEXT("/Game/AnimStarterPack/UE4_Mannequin/Mesh/SK_Mannequin"));
+	if (Skeletal.Succeeded())
+	{
+		//USkeletalMeshComponent* Mesh = GetMesh
+		GetMesh()->SetSkeletalMesh(Skeletal.Object);
+		GetMesh()->SetRelativeLocation(FVector(0.f, 0.f, -90.f));
+		//GetMesh()->SetRelativeRotation(FQuat(FVector(0, 0, 1), -1.57));
+		GetMesh()->SetRelativeRotation(FRotator(0, -90, 0));
+		GetMesh()->SetAnimationMode(EAnimationMode::AnimationSingleNode);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UAnimSequence> WalkAnimation(TEXT("/Game/AnimStarterPack/Walk_Fwd_Rifle_Ironsights"));
+	if (WalkAnimation.Object != NULL)
+	{
+		WalkA = WalkAnimation.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UAnimSequence> StandAnimation(TEXT("/Game/AnimStarterPack/Idle_Rifle_Hip"));
+	if (StandAnimation.Object != NULL)
+	{
+		Stand = StandAnimation.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UAnimSequence> SprintAnimation(TEXT("/Game/AnimStarterPack/Sprint_Fwd_Rifle"));
+	if (SprintAnimation.Object != NULL)
+	{
+		SprintA = SprintAnimation.Object;
+	}
 
 
 	// Create a camera element with position and control
@@ -37,10 +68,40 @@ void AMyCharacter::BeginPlay()
 
 }
 
+bool IsWalking = false;
+bool IsSprint = false;
+bool Sprinting = false;
 // Called every frame
 void AMyCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+
+	if (!GetVelocity().IsNearlyZero()) {
+		if (IsSprint)
+		{
+			if (!Sprinting)
+			{
+				GetMesh()->PlayAnimation(SprintA, true);
+				GetMesh()->SetPlayRate(0.5f);
+				Sprinting = true;
+				IsWalking = false;
+			}
+		}else if (!IsWalking) {
+			GetMesh()->PlayAnimation(WalkA, true);
+			GetMesh()->SetPlayRate(0.5f);
+			IsWalking = true;
+			Sprinting = false;
+		}
+		
+	}
+	else {
+		GetMesh()->PlayAnimation(Stand, true);
+		GetMesh()->SetPlayRate(0.5f);
+		IsWalking = false;
+		Sprinting = false;
+	}
+
 
 }
 
@@ -68,6 +129,7 @@ void AMyCharacter::MoveForward(float InputVector) {
 	// Get the current Direction to move -> In this case Forward or Backward
 	FVector World = GetActorForwardVector();
 
+
 	AddMovementInput(World, InputVector);
 
 }
@@ -82,10 +144,12 @@ void AMyCharacter::MoveSide(float InputVector)
 
 void AMyCharacter::Sprint() {
 	GetCharacterMovement()->MaxWalkSpeed = 1000.f;
+	IsSprint = true;
 }
 
 void AMyCharacter::Walk() {
 	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	IsSprint = false;
 }
 
 void AMyCharacter::Crouched() {
