a782597ccfb1a1e66c2ce7fcebd97b6e482e7346
Stefan Dupont <dupontstef80@gmail.com>
2023-12-20 20:10:16 +0100

    Retryed push

---
+++ Source/Metal_Requiem/EnnemyC/enemyATH.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "enemyATH.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class METAL_REQUIEM_API UenemyATH : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UenemyATH();
+};
